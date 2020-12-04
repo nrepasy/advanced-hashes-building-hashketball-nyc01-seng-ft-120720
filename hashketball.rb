b8df69a3259c9ccf9e437ee31477e442fe367666
nick repasy <nrepasy@gmail.com>
2020-12-04 05:31:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,10 +1,1 @@
-def player_numbers (team)
-  
-arr = []
-  game_hash.each do | key, value|
-    value[:players].each do|key2, value2| 
-                arr << key2[:number]
-    end 
-  end
-   arr
-end
+def game_hash
